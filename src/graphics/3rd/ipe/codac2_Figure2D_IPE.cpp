f0092a56ace7d3e0a177d795cbe9f60a6335cd13
godardma <mael.godard@ensta-bretagne.org>
2025-01-06 15:04:12 +0100

    [graphics] correction for IPE polygon drawing (not closing in some cases

--- src/graphics/3rd/ipe/codac2_Figure2D_IPE.cpp
+++ src/graphics/3rd/ipe/codac2_Figure2D_IPE.cpp
@@ -194,7 +194,20 @@ void Figure2D_IPE::draw_polyline(const std::vector<Vector>& x, float tip_length,
 
 void Figure2D_IPE::draw_polygone(const std::vector<Vector>& x, const StyleProperties& s)
 {
-  draw_polyline(x,0.,s);
+  assert(x.size() > 1);
+  assert(tip_length >= 0.);
+
+  begin_path(s, 0);
+
+  for(size_t k = 0 ; k < x.size() ; k++)
+  {
+    assert(_fig.size() <= x[k].size());
+    _f_temp_content << scale_x(x[k][i()]) << " " << scale_y(x[k][j()]) << ((k == 0 ? " m \n" : " l \n"));
+  }
+
+  _f_temp_content << scale_x(x[0][i()]) << " " << scale_y(x[0][j()]) << " l \n"; // Close the polygon
+
+  _f_temp_content << "</path>";
 }
 
 void Figure2D_IPE::draw_pie(const Vector& c, const Interval& r, const Interval& theta, const StyleProperties& s)
