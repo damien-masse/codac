74e815108b742124e31aec3dcfc8167203430587
godardma <mael.godard@ensta-bretagne.org>
2025-04-24 15:24:32 +0200

    [doc] minor update

--- src/extensions/capd/codac2_capd.h
+++ src/extensions/capd/codac2_capd.h
@@ -3,7 +3,7 @@
  * ----------------------------------------------------------------------------
  *  \date       2024
  *  \author     Maël Godard
- *  \copyright  Copyright 2024 CODAC Team
+ *  \copyright  Copyright 2024 Codac Team
  *  \license    GNU Lesser General Public License (LGPL)
  */
 
@@ -18,50 +18,50 @@
 namespace codac2
 {
   /**
-   * \brief Converts a CODAC Interval object into an CAPD Interval object
+   * \brief Converts a Codac Interval object into an CAPD Interval object
    * 
-   * \param x the CODAC Interval to be converted
+   * \param x the Codac Interval to be converted
    * \return the converted CAPD Interval
    */
   capd::Interval to_capd(const codac2::Interval& x);
 
   /**
-   * \brief Converts an CAPD Interval object into a CODAC Interval object
+   * \brief Converts an CAPD Interval object into a Codac Interval object
    * 
    * \param x the CAPD Interval to be converted
-   * \return the converted CODAC Interval
+   * \return the converted Codac Interval
    */
   codac2::Interval to_codac(const capd::Interval& x);
 
   /**
-   * \brief Converts a CODAC IntervalVector object into an CAPD IVector object
+   * \brief Converts a Codac IntervalVector object into an CAPD IVector object
    * 
-   * \param x the CODAC IntervalVector to be converted
+   * \param x the Codac IntervalVector to be converted
    * \return the converted CAPD IVector
    */
   capd::IVector to_capd(const codac2::IntervalVector& x);
 
   /**
-   * \brief Converts an CAPD IVector object into a CODAC IntervalVector object
+   * \brief Converts an CAPD IVector object into a Codac IntervalVector object
    * 
    * \param x the CAPD IVector to be converted
-   * \return the converted CODAC IntervalVector
+   * \return the converted Codac IntervalVector
    */
   codac2::IntervalVector to_codac(const capd::IVector& x);
 
   /**
-   * \brief Converts a CODAC IntervalMatrix object into an CAPD IMatrix object
+   * \brief Converts a Codac IntervalMatrix object into an CAPD IMatrix object
    * 
-   * \param x the CODAC IntervalMatrix to be converted
+   * \param x the Codac IntervalMatrix to be converted
    * \return the converted CAPD IMatrix
    */
   capd::IMatrix to_capd(const codac2::IntervalMatrix& x);
 
   /**
-   * \brief Converts an CAPD IMatrix object into a CODAC IntervalMatrix object
+   * \brief Converts an CAPD IMatrix object into a Codac IntervalMatrix object
    * 
    * \param x the CAPD IMatrix to be converted
-   * \return the converted CODAC IntervalMatrix
+   * \return the converted Codac IntervalMatrix
    */
   codac2::IntervalMatrix to_codac(const capd::IMatrix& x);
 
