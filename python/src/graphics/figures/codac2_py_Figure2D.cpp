54b6778c6f2acf86801f9b68f1e79aea67bb233b
Simon Rohou <simon.rohou@gmail.com>
2025-05-02 15:53:21 +0200

    [graphics] keeping only draw_polygon from Polygon

--- python/src/graphics/figures/codac2_py_Figure2D.cpp
+++ python/src/graphics/figures/codac2_py_Figure2D.cpp
@@ -141,10 +141,6 @@ void export_Figure2D(py::module& m)
       VOID_FIGURE2D_DRAW_POLYLINE_CONST_VECTOR_VECTOR_REF_FLOAT_CONST_STYLEPROPERTIES_REF,
       "x"_a, "r"_a, "s"_a=StyleProperties())
 
-    .def("draw_polygon", (void(Figure2D::*)(const std::vector<Vector>&,const StyleProperties&))&Figure2D::draw_polygon,
-      VOID_FIGURE2D_DRAW_POLYGON_CONST_VECTOR_VECTOR_REF_CONST_STYLEPROPERTIES_REF,
-      "x"_a, "s"_a=StyleProperties())
-
     .def("draw_polygon", (void(Figure2D::*)(const Polygon&,const StyleProperties&))&Figure2D::draw_polygon,
       VOID_FIGURE2D_DRAW_POLYGON_CONST_POLYGON_REF_CONST_STYLEPROPERTIES_REF,
       "x"_a, "s"_a=StyleProperties())
@@ -286,10 +282,6 @@ void export_Figure2D(py::module& m)
     .def_static("draw_polyline", (void(*)(const std::vector<Vector>&,float,const StyleProperties&))&DefaultFigure::draw_polyline,
       STATIC_VOID_DEFAULTFIGURE_DRAW_POLYLINE_CONST_VECTOR_VECTOR_REF_FLOAT_CONST_STYLEPROPERTIES_REF,
       "x"_a, "r"_a, "s"_a=StyleProperties())
-
-    .def_static("draw_polygon", (void(*)(const std::vector<Vector>&,const StyleProperties&))&DefaultFigure::draw_polygon,
-      STATIC_VOID_DEFAULTFIGURE_DRAW_POLYGON_CONST_VECTOR_VECTOR_REF_CONST_STYLEPROPERTIES_REF,
-      "x"_a, "s"_a=StyleProperties())
 
     .def_static("draw_polygon", (void(*)(const Polygon&,const StyleProperties&))&DefaultFigure::draw_polygon,
       STATIC_VOID_DEFAULTFIGURE_DRAW_POLYGON_CONST_POLYGON_REF_CONST_STYLEPROPERTIES_REF,
