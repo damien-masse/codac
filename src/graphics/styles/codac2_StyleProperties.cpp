bb5899c67901ed9c615eb1f6923d7940ffc4ff9c
godardma <mael.godard@ensta-bretagne.org>
2025-06-07 16:45:51 +0200

    pre merge

--- src/graphics/styles/codac2_StyleProperties.cpp
+++ src/graphics/styles/codac2_StyleProperties.cpp
@@ -15,26 +15,14 @@ using namespace codac2;
 StyleProperties::StyleProperties()
 { }
 
-StyleProperties::StyleProperties(const Color& stroke_color_, const std::string& param1, const std::string& param2)
+StyleProperties::StyleProperties(const Color& stroke_color_, const std::string& param1, const std::string& param2, const std::string& param3)
   : stroke_color(stroke_color_), fill_color(Color::none())
 { 
-  if (param1 != "")
-  {
-    if (StyleProperties::available_line_styles().contains(param1))
-      line_style = param1;
-    else
-      layer = param1;
-  }
-  if (param2 != "")
-  {
-    if (StyleProperties::available_line_styles().contains(param2))
-      line_style = param2;
-    else
-      layer = param2;
-  }
+  bool successful_parsing = update_properties(param1, param2, param3);
+  assert_release(successful_parsing && "Warning: StyleProperties could not parse line width");
 }
 
-StyleProperties::StyleProperties(std::initializer_list<Color> colors, const std::string& param1, const std::string& param2)
+StyleProperties::StyleProperties(std::initializer_list<Color> colors, const std::string& param1, const std::string& param2, const std::string& param3)
   : stroke_color(*colors.begin())
 {
   assert(colors.size() <= 2);
@@ -43,23 +31,49 @@ StyleProperties::StyleProperties(std::initializer_list<Color> colors, const std:
   else
     fill_color = *std::prev(colors.end());
 
-  if (param1 != "")
+  bool successful_parsing = update_properties(param1, param2, param3);
+  assert_release(successful_parsing && "Warning: StyleProperties could not parse line width");
+}
+
+bool is_valid_number(const string& str) 
+{
+  for (char c : str)
+      if (! StyleProperties::available_linewdith_chars().contains(c)) 
+          return false;
+  return true;
+}
+
+bool StyleProperties::parse_parameter(const std::string& param)
+{
+  if (param != "")
   {
-    if (StyleProperties::available_line_styles().contains(param1))
-    line_style = param1;
+    if (StyleProperties::available_line_styles().contains(param))
+      line_style = param;
+
+    else if (is_valid_number(param))
+    {
+      try
+      {
+        line_width = std::stod(param);
+        return true;
+      }
+      catch (const std::invalid_argument& e)
+      {
+        assert_release(false && "Invalid line width value");
+      }
+    }
+
     else
-      layer = param1;
-  }
-  
-  if (param2 != "")
-  {
-    if (StyleProperties::available_line_styles().contains(param2))
-      line_style = param2;
-    else
-      layer = param2;
+      layer = param;
   }
 }
 
+bool StyleProperties::update_properties(const std::string& param1, const std::string& param2, const std::string& param3)
+{
+  return parse_parameter(param1) && 
+         parse_parameter(param2) && 
+         parse_parameter(param3);
+}
 
 
 StyleProperties StyleProperties::inside()
--- src/graphics/styles/codac2_StyleProperties.h
+++ src/graphics/styles/codac2_StyleProperties.h
@@ -27,6 +27,7 @@ namespace codac2
     Color stroke_color = Color::black();
     Color fill_color = Color::none();
     std::string line_style = "-";
+    double line_width = 0.;
     std::string layer = "alpha";
 
     /**
@@ -42,8 +43,9 @@ namespace codac2
      * \param stroke_color Stroke color
      * \param param1 Optional parameter, can be layer name or line style
      * \param param2 Optional parameter, can be layer name or line style
+     * \param param3 Optional parameter, can be layer name or line style
      */
-    StyleProperties(const Color& stroke_color, const std::string& param1 = "", const std::string& param2 = "");
+    StyleProperties(const Color& stroke_color, const std::string& param1 = "", const std::string& param2 = "", const std::string& param3 = "");
 
     /**
      * \brief Constructor from a stroke and eventually a fill color, and two optional parameters
@@ -51,8 +53,27 @@ namespace codac2
      * \param colors list of colors (stroke color and eventually fill color)
      * \param param1 Optional parameter, can be layer name or line style
      * \param param2 Optional parameter, can be layer name or line style
+     * \param param3 Optional parameter, can be layer name or line style
      */
-    StyleProperties(std::initializer_list<Color> colors, const std::string& param1 = "", const std::string& param2 = "");
+    StyleProperties(std::initializer_list<Color> colors, const std::string& param1 = "", const std::string& param2 = "", const std::string& param3 = "");
+
+    /**
+     * \brief Parse a parameter and update the style properties accordingly
+     * 
+     * \param param Parameter to parse, can be a line style, line width or layer name
+     * \return true if the parameter was successfully parsed, false otherwise
+     */
+    bool parse_parameter(const std::string& param);
+
+    /**
+     * \brief Update the style properties from three parameters
+     * 
+     * \param param1 First parameter, can be a line style, line width or layer name
+     * \param param2 Second parameter, can be a line style, line width or layer name
+     * \param param3 Third parameter, can be a line style, line width or layer name
+     * \return true if the parameters were successfully parsed, false otherwise
+     */
+    bool update_properties(const std::string& param1, const std::string& param2, const std::string& param3);
 
     /**
      * \brief Predefined "inside" style, dark gray edge and green fill
@@ -76,6 +97,15 @@ namespace codac2
     {
       std::set<std::string> line_styles={"-", "--", "..", "-.", "-.."};
       return line_styles;
+    };
+
+    /**
+     * \brief Set of available characters for line width
+     */
+    static std::set<char> available_linewdith_chars()
+    {
+      std::set<char> available_chars = {'0','1','2','3','4','5','6','7','8','9','.'};
+      return available_chars;
     };
 
   };
