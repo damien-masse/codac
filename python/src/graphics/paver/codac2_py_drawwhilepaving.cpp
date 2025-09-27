b0901909f0fd239c65c477ad008e3ce45b3dc21b
Simon Rohou <simon.rohou@gmail.com>
2025-09-27 20:42:30 +0200

    [pave] added polar option for draw_while_paving

--- python/src/graphics/paver/codac2_py_drawwhilepaving.cpp
+++ python/src/graphics/paver/codac2_py_drawwhilepaving.cpp
@@ -10,6 +10,7 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/operators.h>
 #include <pybind11/stl.h>
+#include <pybind11/functional.h>
 #include <codac2_Interval.h>
 #include <codac2_IntervalVector.h>
 #include <codac2_drawwhilepaving.h>
@@ -22,11 +23,28 @@ using namespace pybind11::literals;
 
 void export_drawwhilepaving(py::module& m)
 {
-  m.def("draw_while_paving", (void (*)(const IntervalVector&,const CtcBase<IntervalVector>&,double,std::shared_ptr<Figure2D>))&draw_while_paving,
-    VOID_DRAW_WHILE_PAVING_CONST_INTERVALVECTOR_REF_CONST_CTCBASE_INTERVALVECTOR_REF_DOUBLE_SHARED_PTR_FIGURE2D,
+  m.def("cartesian_drawing", &cartesian_drawing);
+  m.def("polar_drawing", &polar_drawing);
+  
+  m.def("draw_while_paving", (void (*)(const IntervalVector& x0, const CtcBase<IntervalVector>& c, double eps,
+      const std::function<void(Figure2D&,const IntervalVector&,const StyleProperties&)>& draw_box,
+      std::shared_ptr<Figure2D> fig)) &draw_while_paving,
+    VOID_DRAW_WHILE_PAVING_CONST_INTERVALVECTOR_REF_CONST_C_REF_DOUBLE_CONST_FUNCTION_VOID_FIGURE2D_REFCONST_INTERVALVECTOR_REFCONST_STYLEPROPERTIES_REF__REF_SHARED_PTR_FIGURE2D,
+    "x0"_a, "c"_a, "eps"_a, "draw_box"_a, "fig"_a=nullptr);
+
+  m.def("draw_while_paving_polar", (void (*)(const IntervalVector& x0, const CtcBase<IntervalVector>& c, double eps,
+      std::shared_ptr<Figure2D> fig)) &draw_while_paving,
+    VOID_DRAW_WHILE_PAVING_CONST_INTERVALVECTOR_REF_CONST_C_REF_DOUBLE_SHARED_PTR_FIGURE2D,
     "x0"_a, "c"_a, "eps"_a, "fig"_a=nullptr);
 
-  m.def("draw_while_paving", (void (*)(const IntervalVector&,const SepBase&,double,std::shared_ptr<Figure2D>))&draw_while_paving,
-    VOID_DRAW_WHILE_PAVING_CONST_INTERVALVECTOR_REF_CONST_SEPBASE_REF_DOUBLE_SHARED_PTR_FIGURE2D,
+  m.def("draw_while_paving", (void (*)(const IntervalVector& x0, const SepBase& s, double eps,
+      const std::function<void(Figure2D&,const IntervalVector&,const StyleProperties&)>& draw_box,
+      std::shared_ptr<Figure2D> fig)) &draw_while_paving,
+    VOID_DRAW_WHILE_PAVING_CONST_INTERVALVECTOR_REF_CONST_S_REF_DOUBLE_CONST_FUNCTION_VOID_FIGURE2D_REFCONST_INTERVALVECTOR_REFCONST_STYLEPROPERTIES_REF__REF_SHARED_PTR_FIGURE2D,
+    "x0"_a, "s"_a, "eps"_a, "draw_box"_a, "fig"_a=nullptr);
+
+  m.def("draw_while_paving_polar", (void (*)(const IntervalVector& x0, const SepBase& s, double eps,
+      std::shared_ptr<Figure2D> fig)) &draw_while_paving,
+    VOID_DRAW_WHILE_PAVING_CONST_INTERVALVECTOR_REF_CONST_S_REF_DOUBLE_SHARED_PTR_FIGURE2D,
     "x0"_a, "s"_a, "eps"_a, "fig"_a=nullptr);
 }
